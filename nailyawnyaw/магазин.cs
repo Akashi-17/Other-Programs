4fa4179220e94d50b36a2817078248ce01bfd2bb
Akashi-17 <78103098+Akashi-17@users.noreply.github.com>
2021-01-27 21:17:40 +0300

    Add files via upload

---
+++ nailyawnyaw/магазин.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nailyawnyaw
+{
+    public delegate void событие();
+    class магазин
+    {
+        public int PointX { get; set; }
+        public int PointY { get; set; }
+        public int SizeX { get; }
+        public int SizeY { get; }
+        public Color цвет { get; set; }
+        public int продажи { get; set; }
+
+        bool готовпродать = false;
+
+        public покупатель покупатель1 { get; set; }
+
+        public event событие выдатьчек;
+
+        public магазин(int PointX1, int PointY1, int SizeX1, int SizeY1,Color цвет1,int продажи1,покупатель покупатель2)
+        {
+            PointX = PointX1;
+            PointY = PointY1;
+            SizeX = SizeX1;
+            SizeY = SizeY1;
+            покупатель1 = покупатель2;
+            цвет = цвет1;
+            продажи = продажи1;
+
+        }
+        public void построить(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.Blue), PointX, PointY, SizeX, SizeY);
+            if (готовпродать == true)
+            {
+                if (продажи <= 10)
+                {
+                    if (цвет == Color.Red)
+                    {
+                        e.Graphics.FillRectangle(new SolidBrush(цвет), PointX, PointY, SizeX, SizeY);
+                        продажи += 1;
+                        цвет = Color.Blue;
+                        System.Threading.Thread.Sleep(500);
+                    }
+                    if (цвет == Color.Blue)
+                    {
+                        e.Graphics.FillRectangle(new SolidBrush(цвет), PointX, PointY, SizeX, SizeY);
+                        продажи += 1;
+                        цвет = Color.Red;
+                        System.Threading.Thread.Sleep(500);
+                    }
+                }
+                else
+                {
+                    выдатьчек();
+                }
+            }
+        }    
+        public void продать()
+        {
+            готовпродать = true;
+        }
+    }
+}
---
+++ nailyawnyaw/покупатель.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nailyawnyaw
+{
+    class покупатель
+    {
+        public int PointX { get; set; }
+        public int PointY { get; set; }
+        public int SizeX { get; }
+        public int SizeY { get; }
+        public int speed { get; set; }
+        public int счетпокупок { get; set; }
+        public bool закупился { get; set; }
+        public Color цвет { get; set; }
+
+        магазин магазин1;
+        
+        public event событие оплата;
+        public покупатель(int PointX1, int PointY2, int SizeX2, int SizeY2, int speed1, Color цвет1,int счётпокупок1,bool закупился1,магазин магазин2)
+        {
+            PointX = PointX1;
+            PointY = PointY2;
+            SizeX = SizeX2;
+            SizeY = SizeY2;
+            speed = speed1;
+            цвет = цвет1;
+            счетпокупок = счётпокупок1;
+            закупился = закупился1;
+            магазин1 = магазин2;
+        }
+        public void пойтивмагазин(object sender, PaintEventArgs e)
+        {
+           
+            if (закупился == false)
+            {
+                if (PointY >= 230)
+                {
+                    PointY -= speed;
+                }
+                else
+                {
+                    if (счетпокупок <= 10)
+                    {
+                        if (цвет == Color.Blue)
+                        {
+                            e.Graphics.FillEllipse(new SolidBrush(цвет), PointX, PointY, SizeX, SizeY);
+                            счетпокупок += 1;
+                            цвет = Color.Red;
+                            System.Threading.Thread.Sleep(500);
+                        }
+                        if (цвет == Color.Red)
+                        {
+                            e.Graphics.FillEllipse(new SolidBrush(цвет), PointX, PointY, SizeX, SizeY);
+                            счетпокупок += 1;
+                            цвет = Color.Blue;
+                            System.Threading.Thread.Sleep(500);
+                        }
+                    }
+                    else
+                    {
+                       оплата();
+                    }
+                }
+            }
+            else
+            {
+                if (PointY >= 0)
+                {
+                    PointY -= speed;
+                }
+            }
+            e.Graphics.DrawEllipse(new Pen(Color.Red), PointX, PointY, SizeX, SizeY);
+        }
+        public void уйти()
+        {
+            закупился = true;
+        }
+    }
+}
